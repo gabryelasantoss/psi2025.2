bf5771d23460123323064b6051e69908d0578967
gabryelasantoss <160539301+gabryelasantoss@users.noreply.github.com>
2025-09-16 20:37:47 +0000

    aula 2 - rotas

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask #importando biblioteca 
+from flask import Flask, render_template #importando biblioteca 
 app = Flask(__name__) 
 
 @app.route('/') #@ significa anotação / route=rota / "/" signfica a rota raíz
@@ -9,6 +9,13 @@ def index(): #criando método
 def contato():
   return 'alba.lopes@ifrn.edu.br'
 
+@app.route('/exemplo')
+def exemplo():
+  return render_template('exemplo.html')
+
+@app.route('/exemplo2')
+def exemplo2():
+   return render_template('exemplo2.html')
 
 if __name__ == '__main__':
     app.run()
